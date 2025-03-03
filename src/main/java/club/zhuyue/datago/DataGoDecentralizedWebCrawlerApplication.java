de0d59198c975c588205d56b2023ed618105931c
chasemydrea <2196018156@qq.com>
2025-03-03 15:09:00 +0800

    字节寒假大数据青训营项目2-简易分布式爬虫系统实现，推送项目

---
+++ src/main/java/club/zhuyue/datago/DataGoDecentralizedWebCrawlerApplication.java
@@ -0,0 +1,13 @@
+package club.zhuyue.datago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGoDecentralizedWebCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataGoDecentralizedWebCrawlerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/club/zhuyue/datago/DataGoDecentralizedWebCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package club.zhuyue.datago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGoDecentralizedWebCrawlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
